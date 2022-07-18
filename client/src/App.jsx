f4ee2203e12ce47e28dd5c504c6a2e87e34687f8
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-07-18 09:15:26 +0700

    base project! ready to code

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab culpa cumque,
+      cupiditate delectus dolore eligendi enim et iusto minus, necessitatibus
+      nihil nobis officia pariatur possimus quisquam tempore totam unde veniam?
+    </h1>
+  );
+};
+
+export default App;
