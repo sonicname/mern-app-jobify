950dfdf2115f62482eb9f5dcee6235176ddd7421
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-07-19 08:57:47 +0700

    update user model

---
+++ server/models/User.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email",
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minLength: 6,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxLength: 20,
+    default: "last name",
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxLength: 20,
+    default: "my city",
+  },
+});
+
+export default mongoose.model("User", UserSchema);
