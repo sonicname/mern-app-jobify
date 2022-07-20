fcc554d78c89ff034c22984aa5e34628b8b17b5f
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-07-20 15:26:07 +0700

    update login frontend

--- client/src/context/reducers.jsx
+++ client/src/context/reducers.jsx
@@ -1,24 +1,64 @@
-import { CLEAR_ALERT, DISPLAY_ALERT } from "./actions.jsx";
+import {
+  CLEAR_ALERT,
+  DISPLAY_ALERT,
+  SETUP_USER_BEGIN,
+  SETUP_USER_ERROR,
+  SETUP_USER_SUCCESS,
+} from "./actions.jsx";
 
 const reducers = (state, action) => {
-  if (action.type === DISPLAY_ALERT) {
-    return {
-      ...state,
-      showAlert: true,
-      alertType: "danger",
-      alertText: "Please provide all values!",
-    };
-  }
+  switch (action.type) {
+    case DISPLAY_ALERT: {
+      return {
+        ...state,
+        showAlert: true,
+        alertType: "danger",
+        alertText: "Please provide all values!",
+      };
+    }
 
-  if (action.type === CLEAR_ALERT) {
-    return {
-      ...state,
-      showAlert: false,
-      alertType: "",
-      alertText: "",
-    };
+    case CLEAR_ALERT: {
+      return {
+        ...state,
+        showAlert: false,
+        alertType: "",
+        alertText: "",
+      };
+    }
+
+    case SETUP_USER_BEGIN: {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    }
+
+    case SETUP_USER_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        token: action.payload.token,
+        user: action.payload.user,
+        userLocation: action.payload.location,
+        jobLocation: action.payload.location,
+        showAlert: true,
+        alertType: "success",
+        alertText: action.payload.alertText,
+      };
+    }
+
+    case SETUP_USER_ERROR: {
+      return {
+        ...state,
+        isLoading: false,
+        showAlert: true,
+        alertType: "danger",
+        alertText: action.payload.message,
+      };
+    }
+    default:
+      throw new Error(`no such action: ${action.type}`);
   }
-  throw new Error(`no such action: ${action.type}`);
 };
 
 export default reducers;
