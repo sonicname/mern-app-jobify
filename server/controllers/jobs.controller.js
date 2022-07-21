ca9557a4aacf98f070d236cad8f8a62811ac83d0
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-07-22 03:05:49 +0700

    update job model and route create job

--- server/controllers/jobs.controller.js
+++ server/controllers/jobs.controller.js
@@ -1,5 +1,18 @@
+import { BadRequestError } from "../errors/CustomAPIError.js";
+import { StatusCodes } from "http-status-codes";
+import Job from "../models/Job.js";
+
 const createJob = async (req, res) => {
-  res.send("create job");
+  const { position, company } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError("Please Provide All Values");
+  }
+
+  req.body.createdBy = req.user.userId;
+
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const getAllJobs = async (req, res) => {
