7d390ef63b5e80575a3101e5ea2f54d7be3f841e
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-07-22 03:24:15 +0700

    update add job

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -5,5 +5,15 @@ import BigSidebar from "./BigSidebar.jsx";
 import Navbar from "./Navbar.jsx";
 import SmallSidebar from "./SmallSidebar.jsx";
 import NavLinks from "./NavLinks.jsx";
+import FormRowSelect from "./FormRowSelect.jsx";
 
-export { Logo, FormRow, BigSidebar, Alert, Navbar, NavLinks, SmallSidebar };
+export {
+  Logo,
+  FormRow,
+  BigSidebar,
+  Alert,
+  Navbar,
+  NavLinks,
+  SmallSidebar,
+  FormRowSelect,
+};
--- server/controllers/jobs.controller.js
+++ server/controllers/jobs.controller.js
@@ -12,11 +12,15 @@ const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
 
   const job = await Job.create(req.body);
-  res.status(StatusCodes.CREATED).json({ job });
+  return res.status(StatusCodes.CREATED).json({ job });
 };
 
 const getAllJobs = async (req, res) => {
-  res.send("create job");
+  const jobs = await Job.find({ createdBy: req.user.userId });
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
 
 const updateJob = async (req, res) => {
